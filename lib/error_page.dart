b0b3686b37200112d2b2f9aad695a260631fec85
msyamsularif <syamsularif0904@gmail.com>
2023-09-21 12:08:37 +0700

    first commit deeplink

---
+++ lib/error_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Halaman Tidak Ditemukan',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                context.go('/');
+              },
+              child: const Text('Home Page'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
